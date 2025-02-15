1374a3725de7d554ec932c8f3c5d23d7755bb64b
riomar0001 <inguitomario00@gmail.com>
2025-02-16 00:33:51 +0800

    Routes and Middleware Creations
    - Create Applicant and Company Auth Handler Middleware
    - Create Applicant, Company, and Jobs route file
    - Create Upload Logo, Profile Picture, Resume Handler

---
+++ backend/routes/applicantRoutes.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { upload as uploadProfilePicture } from "@/middlewares/uploadProfilePictureHandler";
+import { upload as uploadResume } from "@/middlewares/uploadResumeHandler";
+
+const router = express.Router();
+
+/**
+ * @Reminder Place all GET requests here
+ * @Format router.get("path", "middleware", "controller");
+ */
+
+/**
+ * @Reminder Place all POST requests here
+ * @Format router.post("path", "middleware", "controller");
+ */
+
+/**
+ * @Reminder Place all PUT requests here
+ * @Format router.put("path", "middleware", "controller");
+ */
+
+/**
+ * @Reminder Place all DELETE requests here
+ * @Format router.delete("path", "middleware", "controller");
+ */
+
+export default router;
