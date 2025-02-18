1171bc883ee92beb4a071d84ef5cfb523bb47c3e
ykylejan <jkDellatan@mcm.edu.ph>
2025-02-18 23:03:17 +0800

    Add Resume (small) card to Profiles

--- frontend/src/pages/applicant/Profile.tsx
+++ frontend/src/pages/applicant/Profile.tsx
@@ -6,10 +6,11 @@ import CareerStatus from "@/components/customs/applicant/CareerStatus"
 import Education from "@/components/customs/applicant/Education"
 import PersonalInformations from "@/components/customs/applicant/PersonalInformations"
 import Resume from "@/components/customs/applicant/Resume"
+import ResumeSmall from "@/components/customs/applicant/ResumeSmall"
 
 const Profile = () => {
     return (
-        <div className="bg-neutral-100 flex">
+        <div className="bg-neutral-100 flex justify-center gap-x-8 px-24 py-12">
 
             <div className="space-y-8">
                 <AccountHead />
@@ -23,6 +24,7 @@ const Profile = () => {
                 <ActivePositions />
                 <CareerStatus />
                 <PersonalInformations />
+                <ResumeSmall />
             </div>
 
 
