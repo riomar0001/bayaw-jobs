e64ee388795e13e5d6ea7809205c78242b2ec8a1
Albert Pueso <adrianputian12@gmail.com>
2025-02-19 00:17:15 +0800

    minor fix in field

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,28 @@
 import {
-    Route,
-    createBrowserRouter,
-    createRoutesFromElements,
-    RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Landing from "./pages/Landing";
 import Profile from "./pages/applicant/Profile";
-
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<MainLayout />}>
-            <Route index element={<Landing />} />
-            <Route path="/applicant/profile" element={<Profile />} />
-        </Route>
-    )
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Landing />} />
+      <Route path="/applicant/profile" element={<Profile />} />
+      <Route path="/register" element={<Registration />} />
+      <Route path="/login" element={<Login />} />
+    </Route>
+  )
 );
 
 const App = () => {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
