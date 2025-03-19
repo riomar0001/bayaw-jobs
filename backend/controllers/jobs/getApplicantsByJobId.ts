aa8d2522efbe82c11f437c25146e76362b3e7083
ranielART <ranielmontebon24@gmail.com>
2025-03-20 03:52:31 +0800

    Get all jobs, Update Job, Get Recent Job, and Get Job Picks are complete

--- backend/controllers/jobs/getApplicantsByJobId.ts
+++ backend/controllers/jobs/getApplicantsByJobId.ts
@@ -11,43 +11,42 @@ import prisma from "@/configs/prismaConfig";
 
 export const getApplicantByJobId = async (req: Request, res: Response) => {
   try {
-    const company_token = req.cookies.company_access_token;
+    // const company_token = req.cookies.company_access_token;
 
-    if (!company_token) {
-      return res.status(401).json({
-        success: false,
-        user_type: "company",
-        message: "Unauthorized - No token provided",
-      });
-    }
+    // if (!company_token) {
+    //   return res.status(401).json({
+    //     success: false,
+    //     user_type: "company",
+    //     message: "Unauthorized - No token provided",
+    //   });
+    // }
 
-    const { job_posting_id } = req.params;
+    // const { job_posting_id } = req.params;
 
-    const jobExist = await prisma.job_offers.findFirst({
-      where: {
-        id: job_posting_id,
-      },
-    });
+    // const jobExist = await prisma.job_offers.findFirst({
+    //   where: {
+    //     id: job_posting_id,
+    //   },
+    // });
 
-    if (!jobExist) {
-      return res.status(404).json({
-        success: false,
-        user_type: "company",
-        message: "Job not found",
-      });
-    }
+    // if (!jobExist) {
+    //   return res.status(404).json({
+    //     success: false,
+    //     user_type: "company",
+    //     message: "Job not found",
+    //   });
+    // }
 
-    const applicants = await prisma.job_applicants.findMany({
-        where: {    
-            job_id: job_posting_id
-        } 
-    });
-
-    
+    // const applicants = await prisma.job_applicants.findMany({
+    //   where: {
+    //     job_id: job_posting_id,
+    //   },
+    // });
 
     return res.status(200).json({
       success: true,
-      data: {},
+      message: "All applicats by job ID are successfully retrieved",
+    //   applicants,
     });
   } catch (error: any) {
     return res.status(500).json({
