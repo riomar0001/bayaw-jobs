31bec7283693f46c7ffccee06e2d485d9a60095a
ranielART <ranielmontebon24@gmail.com>
2025-03-20 17:16:16 +0800

    Retrive applicants by job id is complete but needs improvement

--- backend/controllers/jobs/getApplicantsByJobId.ts
+++ backend/controllers/jobs/getApplicantsByJobId.ts
@@ -11,42 +11,85 @@ import prisma from "@/configs/prismaConfig";
 
 export const getApplicantByJobId = async (req: Request, res: Response) => {
   try {
-    // const company_token = req.cookies.company_access_token;
+    const company_token = req.cookies.company_access_token;
 
-    // if (!company_token) {
-    //   return res.status(401).json({
-    //     success: false,
-    //     user_type: "company",
-    //     message: "Unauthorized - No token provided",
-    //   });
-    // }
+    if (!company_token) {
+      return res.status(401).json({
+        success: false,
+        user_type: "company",
+        message: "Unauthorized - No token provided",
+      });
+    }
 
-    // const { job_posting_id } = req.params;
+    const { job_posting_id } = req.params;
 
-    // const jobExist = await prisma.job_offers.findFirst({
-    //   where: {
-    //     id: job_posting_id,
-    //   },
-    // });
+    const jobExist = await prisma.job_offers.findFirst({
+      where: {
+        id: job_posting_id,
+      },
+    });
 
-    // if (!jobExist) {
-    //   return res.status(404).json({
-    //     success: false,
-    //     user_type: "company",
-    //     message: "Job not found",
-    //   });
-    // }
+    if (!jobExist) {
+      return res.status(404).json({
+        success: false,
+        user_type: "company",
+        message: "Job not found",
+      });
+    }
 
-    // const applicants = await prisma.job_applicants.findMany({
-    //   where: {
-    //     job_id: job_posting_id,
-    //   },
-    // });
+    const applicantList = await prisma.job_applicants.findMany({
+      where: {
+        job_id: job_posting_id,
+      },
+      include: {
+        applicants: {
+          select: {
+            username: true,
+            email: true,
+            created_at: true,
+            applicants_personal_information: {
+              select: {
+                first_name: true,
+                last_name: true,
+                contact_no: true
+              }
+            },
+            applicants_experience: true,
+            applicants_resume: {
+              select: {
+                resume_file: true
+              }
+            }
+          }
+        }
+      }
+    });
+    
+    
+
+    // console.log("APPLICANTS:", applicants);
+    if(!applicantList) {
+      return res.status(404).json({
+        success: false,
+        user_type: "company",
+        message: "Applicants not found",
+      });
+    }
+
+    // console.log(JSON.stringify(applicantList, null, 2));
+
+    // const applicantMap = {};
+    
+    applicantList.forEach((applicant) => {
+      // const {} = applicant;
+      console.log(JSON.stringify(applicant, null, 2));
+      // console.log(JSON.stringify(applicant.applicants.applicants_experience, null, 2));
+    });
 
     return res.status(200).json({
       success: true,
       message: "All applicats by job ID are successfully retrieved",
-    //   applicants,
+      applicantList,  
     });
   } catch (error: any) {
     return res.status(500).json({
