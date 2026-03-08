6929ff0303cf9d98e6d8570e4a634a9c8a194c8b
riomar0001 <mariojringuito@riomar.dev>
2026-03-09 00:29:32 +0800

    fix(prisma): correct pg import path
    - Updated the import statement for pg to use the correct package path.
    - Ensures compatibility with the Prisma adapter for PostgreSQL.

--- server/src/configs/prisma.config.ts
+++ server/src/configs/prisma.config.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import pg from '../../node_modules/@types/pg';
+import pg from 'pg';
 
 const connectionString = process.env.DIRECT_URL ?? process.env.DATABASE_URL;
 
